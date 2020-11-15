2c68440c952fddd6ea553e91737a872e7be0f299
dvkndn <dvkndn@gmail.com>
2020-11-15 16:32:17 +0700

    Build: Add Moai kit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
+import { scrollbar } from "@moai/core";
+
+console.log(scrollbar.custom);
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
 	return <Component {...pageProps} />;
