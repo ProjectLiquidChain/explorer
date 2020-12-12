662ced3f5ab5f546ef1f74a67a12ae9aade255d1
dvkndn <dvkndn@gmail.com>
2020-12-12 22:20:42 +0700

    Fix: Prevent showing account creator and storage hash in user account

--- components/account/overview/overview.tsx
+++ components/account/overview/overview.tsx
@@ -11,32 +11,32 @@ interface Props {
 
 export const AccountOverview = ({ account }: Props) => (
 	<Pane>
+		<Info
+			label="Account Nonce"
+			help="Sequential running number of this address, beginning with 0 for the first transaction. For example, if the nonce is 10, the address has sent 11 transactions."
+			children={<Numeric type="integer" value={account.nonce} />}
+		/>
 		{isContractAccount(account) && (
 			<>
+				<Divider />
 				<Info
 					label="Contract hash"
 					copy={account.contractHash}
 					children={account.contractHash}
 				/>
 				<Divider />
+				<Info
+					label="Account creator"
+					copy={account.creator}
+					children={<AccountAddress wrap value={account.creator} />}
+				/>
+				<Divider />
+				<Info
+					label="Storage hash"
+					copy={account.storageHash}
+					children={account.storageHash}
+				/>
 			</>
 		)}
-		<Info
-			label="Account Nonce"
-			help="Sequential running number of this address, beginning with 0 for the first transaction. For example, if the nonce is 10, the address has sent 11 transactions."
-			children={<Numeric type="integer" value={account.nonce} />}
-		/>
-		<Divider />
-		<Info
-			label="Account creator"
-			copy={account.creator}
-			children={<AccountAddress wrap value={account.creator} />}
-		/>
-		<Divider />
-		<Info
-			label="Storage hash"
-			copy={account.storageHash}
-			children={account.storageHash}
-		/>
 	</Pane>
 );
