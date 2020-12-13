ea877a314750ae4001bc1eea3aac104fed6a4b6c
dvkndn <dvkndn@gmail.com>
2020-12-13 23:48:29 +0700

    Fix: Fix horizontal scroll at body

--- components/page/header/header.tsx
+++ components/page/header/header.tsx
@@ -1,20 +1,20 @@
 import { container } from "@/components/container/container";
-import { Pane } from "@moai/core";
-import { DivPx } from "@moai/core";
+import { background, Border, borderColor, boxShadow, DivPx } from "@moai/core";
 import s from "./header.module.css";
 import { Logo } from "./logo/logo";
 import { Search } from "./search/search";
 import { Theme } from "./theme/theme";
 
 export const PageHeader = () => (
-	<div className={s.container}>
-		<Pane noPadding>
-			<div className={[s.body, container.max960].join(" ")}>
-				<div className={s.logo} children={<Logo />} />
-				<div className={s.search} children={<Search />} />
-				<DivPx size={16} />
-				<div className={s.theme} children={<Theme />} />
-			</div>
-		</Pane>
+	<div
+		className={[s.container, boxShadow.strong, background.primary].join(" ")}
+	>
+		<div className={[s.body, container.max960].join(" ")}>
+			<div className={s.logo} children={<Logo />} />
+			<div className={s.search} children={<Search />} />
+			<DivPx size={16} />
+			<div className={s.theme} children={<Theme />} />
+		</div>
+		<Border color="weak" />
 	</div>
 );
